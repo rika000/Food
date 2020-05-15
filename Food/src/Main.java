f7d114d90afd527c01df8206b7c4bd7a8b6de412
lika <lika@172.20.10.2>
2020-05-15 17:06:08 +0900

    ソフトウェア開発法、課題

---
+++ Food/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		System.out.println("好きな食べ物：ケーキ");
+		System.out.println("理由：祝い事の時に食べることが多いため、特別な感じがするから");
+
+	}
+
+}
